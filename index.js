ec74a2e78b415e0a7cae65af6ac57640932370cb
sunpy1106 <sunpy1106@qq.com>
2023-06-18 23:14:47 +0800

    modify the index.js

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ exports.handler = async (event) => {
 
   // Use Promise to make sure the unzip process is done before ending the lambda function
   await new Promise((resolve, reject) => {
+    // Initialize the tree structure
+    let tree = {};
     s3Stream
       .on("error", (error) => reject(`Error with s3Stream: ${error}`))
       .pipe(writeStream)
@@ -43,22 +45,79 @@ exports.handler = async (event) => {
               `Processing entry - fileName: ${fileName}, type: ${type}, size: ${size}`
             );
 
-            if (type === "File") {
+            if (
+              fileName.startsWith("__MACOSX/") ||
+              fileName.endsWith(".DS_Store")
+            ) {
+              console.log(`Ignoring system file: ${fileName}`);
+              entry.autodrain();
+            } else if (type === "File") {
               console.log(`Uploading file: ${fileName} to bucket: ${Bucket}`);
-              await s3
+
+              // Upload file to S3
+              const uploadResult = await s3
                 .upload({
                   Bucket,
                   Key: `${newKey}/${fileName}`,
                   Body: entry,
                 })
                 .promise();
+
+              // Create product and sku object
+              const fileParts = path.dirname(fileName).split("/");
+              const collectionName = fileParts[1];
+              const productName = fileParts[2];
+              const skuName = path.basename(fileName, path.extname(fileName));
+
+              const sku = {
+                skuTitle: skuName,
+                imageUrl: uploadResult.Location,
+              };
+
+              // Initialize the collection if it doesn't exist
+              if (!tree[collectionName]) {
+                tree[collectionName] = {
+                  collection: collectionName,
+                  products: [],
+                };
+              }
+
+              // Find the product this sku belongs to
+              let productNode = tree[collectionName].products.find(
+                (product) => product.title === productName
+              );
+
+              // If product doesn't exist, create a new one
+              if (!productNode) {
+                productNode = { title: productName, sku: [] };
+                tree[collectionName].products.push(productNode);
+              }
+
+              // Add the sku to the product
+              productNode.sku.push(sku);
+
               console.log(`Uploaded file: ${fileName} to bucket: ${Bucket}`);
             } else {
               console.log(`Ignoring directory: ${fileName}`);
               entry.autodrain();
             }
           })
-          .on("finish", () => resolve()); // Resolve the promise when unzip process is finished
+          .on("finish", async () => {
+            // Convert the tree to an array of collections and write it into A.OK and upload it to S3
+            const collections = Object.values(tree);
+            const treeKey = `${newKey}/A.OK`;
+            const treeBody = JSON.stringify(collections);
+            await s3
+              .upload({
+                Bucket,
+                Key: treeKey,
+                Body: treeBody,
+              })
+              .promise();
+
+            console.log(`Uploaded A.OK to bucket: ${Bucket}`);
+            resolve(); // Resolve the promise when unzip process is finished and A.OK is uploaded
+          });
       });
   });
 
